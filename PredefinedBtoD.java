672666bf62632fb33efd58480fe2d1c27963c327
RMishra619 <115406604+RMishra619@users.noreply.github.com>
2022-10-09 18:22:37 +0530

    Binary to Decimal
    Using predefined function to convert binary to decimal

---
+++ PredefinedBtoD.java
@@ -0,0 +1,10 @@
+class PredefinedBtoD{
+    public static void main(String[] args) {
+        String BinaryNumber = "1001";
+        System.out.println(Integer.parseInt(BinaryNumber,2 ));
+       
+        System.out.println(Integer.parseInt(BinaryNumber,8 ));
+       
+    }
+    
+}
